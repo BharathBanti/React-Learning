7272b21d384f40f62e8667bfe1aa132fe626c4ea
BharathBanti <dasarib180@gmail.com>
2026-03-20 15:43:32 +0530

    Paused

--- Instagram/src/App.jsx
+++ Instagram/src/App.jsx
@@ -1,23 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
-function Navbar() {
-  return <div className="block navbar">Header</div>;
-}
-
-function Card() {
-  return <div className="block card">Card</div>;
-}
-
-function Footer() {
-  return <div className="block footer">Footer</div>;
-}
-
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleCount(){
+    return setCount(prevCount => prevCount + 1);
+  }
+
+  useEffect(() =>
+    console.log('Inside useEffect'), [count]
+  )
+
   return (
     <div className="app">
-      <Navbar />
-      <Card />
-      <Footer />
+      <button onClick={handleCount}>{count}</button>
     </div>
   );
 }
