121303f4a9f0a19650f3bc86ae9c22b7b6399be3
BharathBanti <dasarib180@gmail.com>
2026-03-25 09:21:35 +0530

    Started Working

---
+++ WorldWise/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+  );
+}
+
+export default App;
